af85eb7a50008aacb615c9c47147c87b03dbb0a1
Clarence-Kinuthia <mclarence7311@gmail.com>
2024-03-28 16:46:32 +0300

    array find  method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(arr){
+
+   let ans =  arr.find((el) => {
+        if(el.result === "W"){
+            return el.year
+        }
+    })
+
+    return ans ? ans.year : undefined
+
+    
+}
+
+
+
+// record.find(superBowlWin)
